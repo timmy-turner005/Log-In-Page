b98e96c0617a3e9643319a26815294198aa201c9
timmy-turner005 <timitalented005@gmail.com>
2024-06-11 15:09:24 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AuthInputs from "./components/AuthInputs";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AuthInputs />
+    </>
+  );
+}
+
+export default App;
